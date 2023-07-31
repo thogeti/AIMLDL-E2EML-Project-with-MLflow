ce848dd41dc8f5d71c92a55443cd3268e8d6d54a
thogeti <thogeti.dev@gmail.com>
2023-07-31 17:12:41 +0530

    data ingestion

--- src/mlProject/constants/__init__.py
+++ src/mlProject/constants/__init__.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
-SCHEMA_FILE_PATH = Path("schema.yaml")
+#
+CONFIG_FILE_PATH = Path("C:\\Praveen\\Work\\AIMLDL\\VSProjects\\AIMLDL-E2EML-Project-with-MLflow\\config\\config.yaml")
+
+PARAMS_FILE_PATH = Path("C:\\Praveen\\Work\\AIMLDL\\VSProjects\\AIMLDL-E2EML-Project-with-MLflow\\params.yaml")
+SCHEMA_FILE_PATH = Path("C:\\Praveen\\Work\\AIMLDL\\VSProjects\\AIMLDL-E2EML-Project-with-MLflow\\schema.yaml")
+##CONFIG_FILE_PATH = Path("config/config.yaml")
